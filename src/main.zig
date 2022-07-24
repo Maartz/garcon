a40b0cbe963d581b14ac364519713f6894e65d2c
William Martin <martin@trouveunveto.fr>
2022-07-24 19:19:46 +0200

    pass mode to evented, thus async

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const net = std.net;
 const StreamServer = net.StreamServer;
 const Address = net.Address;
+pub const io_mode = .evented;
 
 pub fn main() anyerror!void {
     const stream_server = StreamServer.init(.{});
